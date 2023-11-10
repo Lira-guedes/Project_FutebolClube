267a399d19e7fe067d788a0def2b2b80893f1738
Lira Guedes <liracrg@gmail.com>
2023-11-09 21:18:57 -0300

    feat: req 2

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -1,45 +1,16 @@
-import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-
-import { app } from '../app';
-import Example from '../database/models/ExampleModel';
-
-import { Response } from 'superagent';
+import { App } from '../app';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
+const app = new App().app
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+describe('Route test', () => {
+    it('Testa se Route / funciona', async () => {
+      const res = await chai.request(app).get('/');
+      expect(res.body).to.deep.equal({ ok: true });
+    });
 });
---
+++ app/backend/src/tests/team.test.ts
@@ -0,0 +1,27 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import { App } from '../app';
+import SequelizeTeam from '../database/models/sequelizeTeam';
+
+// @ts-ignore
+import chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const team = {
+  id: 1,
+  teamName: 'Cruzeiro',
+}
+const teams = [team];
+
+describe('Teams', () => {
+  it('Retorna teams', async () => {
+    sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any);
+
+    const { status, body } = await chai.request(app).get('/teams');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(teams);
+  });
+});
