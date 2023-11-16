7f1d76dc3de27b49334eeec5483d5d39baee2970
Lira Guedes <liracrg@gmail.com>
2023-11-16 17:57:08 -0300

    feat: req 24 e 25

--- app/backend/src/service/Leaderboard.service.ts
+++ app/backend/src/service/Leaderboard.service.ts
@@ -8,7 +8,23 @@ export default class LeaderboardService {
   ) { }
 
   public async getAll(): Promise<ServiceResponse<ILeaderboard[]>> {
-    const leaderboard = await this.leaderboardModel.getHome();
-    return { status: 'SUCCESSFUL', data: leaderboard };
+    const leaderHome = await this.leaderboardModel.getHome();
+    const leaderboard = leaderHome.map((item) => (
+      { ...item,
+        goalsBalance: item.goalsFavor - item.goalsOwn,
+        efficiency: ((item.totalPoints / (item.totalGames * 3)) * 100).toFixed(2),
+      }));
+    const scoreLeaderboard = leaderboard.sort((a, b) => {
+      if (a.totalPoints > b.totalPoints) return -1;
+      if (a.totalPoints < b.totalPoints) return 1;
+      if (a.totalVictories > b.totalVictories) return -1;
+      if (a.totalVictories < b.totalVictories) return 1;
+      if (a.goalsBalance > b.goalsBalance) return -1;
+      if (a.goalsBalance < b.goalsBalance) return 1;
+      if (a.goalsFavor > b.goalsFavor) return -1;
+      if (a.goalsFavor < b.goalsFavor) return 1;
+      return 0;
+    });
+    return { status: 'SUCCESSFUL', data: scoreLeaderboard };
   }
 }
